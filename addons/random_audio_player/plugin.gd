f4a45756b2bc1c45f6daff2f1aa0941e0868ec2c
Wenzel <konrad@labk.it>
2022-08-19 10:50:43 +0200

    Removed add_custom_type import for alpha 14

--- addons/random_audio_player/plugin.gd
+++ addons/random_audio_player/plugin.gd
@@ -1,5 +1,2 @@
 @tool
 extends EditorPlugin
- 
-func _enter_tree():
-	add_custom_type("RandomAudioPlayer", "Node2D", preload("./nodes/random_audio_player.gd"), preload("./nodes/random_audio_player.svg"))
